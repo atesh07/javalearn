d7b21e2ff9bcee20b93a7c8cf070ac5ce8923606
ehtesham.2002.aw@gmail.com <ehesham.2002.aw@gmail.com>
2026-03-27 10:49:00 +0530

    day2

---
+++ day2/string1.java
@@ -0,0 +1,18 @@
+public class string1{
+    public static void main(String[] arges){
+        String name = "Ehtesham Anwar";
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.indexOf("Anwar"));
+        System.out.println(name.charAt(0));
+        String name2 = " saad khan.  "; 
+        System.out.println(name2.trim());
+        System.out.println(name2);
+       //for check both string are same or not
+        System.out.println(name2.equals(name));
+        
+
+
+    }
+}
