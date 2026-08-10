4d7c08d1e4d60a9cd042da86b08e7d289d578c69
ehtesham.2002.aw@gmail.com <ehesham.2002.aw@gmail.com>
2026-08-10 22:36:58 +0530

    concatination

--- day5/string.java
+++ day5/string.java
@@ -1,9 +1,12 @@
 public class string{
     public static void main(String[] args){
-        String name = "Ehtesham Anwar";
+        String name = "Ehtesham";
         String a="apple";
         String b="banana";
+        String ab="1";
+        int x=1;
         System.out.println(a.equals(b));
+        System.out.println(ab.equals(String.valueOf(x)));
         System.out.println(a.compareTo(b));
         // System.out.println(name);
         // System.out.println(name.length());
@@ -12,6 +15,14 @@ public class string{
         // System.out.println(name.charAt(0));
         // System.out.println(name.indexOf("A"));
         // System.out.println(name.lastIndexOf("a"));
+        System.out.println(name.replace("Ehtesham", "Ali"));
+        String name1="    nishi    ";
+        System.out.println(name1.trim());
+        String hehe="weds";
 
+     // concatination rules 
+     System.out.println(name+ ""+name1.trim());
+     System.out.println(name.concat(name1.trim()));
+     System.out.println(name.concat(hehe).concat(name1.trim()));
     }
 }
