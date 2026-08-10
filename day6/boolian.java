96d20c0e1cdf5ec8922f961c2090232cc273ce56
ehtesham.2002.aw@gmail.com <ehesham.2002.aw@gmail.com>
2026-08-10 23:45:50 +0530

    some question done star promblem

--- day6/boolian.java
+++ day6/boolian.java
@@ -1,17 +1,36 @@
 public class  boolian{
     public static void main(String[] args){
-        int age = 20;
-        if(age>=18){
-            System.out.println("You are eligible to vote");
-        }
-        else{
-            System.out.println("You are not eligible to vote");  
+    //     int age = 20;
+    //     if(age>=18){
+    //         System.out.println("You are eligible to vote");
+    //     }
+    //     else{
+    //         System.out.println("You are not eligible to vote");  
+    //     }
+
+    //  String result = (age>=18) ? "you are eligible to vote"+ " "+ age: "you are not eligible to vote";
+    //  System.out.println(result);       
+
+        for(int i=0; i<10;i++){
+            for(int j=0;j<10;j++){
+                if((j==0 || i==9 || j ==9)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
         }
 
-     String result = (age>=18) ? "you are eligible to vote"+ " "+ age: "you are not eligible to vote";
-     System.out.println(result);       
+        for(int i = 0; i<9; i++){
+            for(int j = 0; j < i; j++){
+                if()
+            }
+        }
+    
 
- 
+
+
     }
 
     }
