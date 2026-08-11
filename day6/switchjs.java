aa894aab2f93b6afe8ebe73210007c6f44b3c225
ehtesham.2002.aw@gmail.com <ehesham.2002.aw@gmail.com>
2026-08-11 22:27:07 +0530

    switch

---
+++ day6/switchjs.java
@@ -0,0 +1,32 @@
+public class switchjs{
+    public static void main(String[] arges){
+        int day = 1;
+        System.out.println("hello world");
+        switch(day){
+            case 1:
+                System.out.println("Hello Monday");
+                break;
+            case 2:
+                System.out.println("hello tues");
+                break;
+            case 3:
+                System.out.println("hello wed");
+                break;
+            case 4:
+                System.out.println("hello thus");
+                break;
+            case 5 :
+                System.out.println("hello friday");
+                break;
+            case 6 :
+                System.out.println("sturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default :
+            System.out.println("invlid entry");
+                   
+                    }
+    }
+}
